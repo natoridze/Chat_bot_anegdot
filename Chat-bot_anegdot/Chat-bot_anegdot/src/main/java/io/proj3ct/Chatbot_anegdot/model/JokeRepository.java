17b1ca78696fd2d926f282df05aa9690beaedf0f
natoridze <161938117+natoridze@users.noreply.github.com>
2024-05-28 00:40:21 +0300

    Delete Chat-bot_anegdot/Chat-bot_anegdot/src/main/java/io/proj3ct/Chatbot_anegdot directory

--- Chat-bot_anegdot/Chat-bot_anegdot/src/main/java/io/proj3ct/Chatbot_anegdot/model/JokeRepository.java
+++
@@ -1,9 +0,0 @@
-package io.proj3ct.Chatbot_anegdot.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface JokeRepository extends CrudRepository<Joke, Integer> {
-
-    Integer findMaxId();
-}
